8275c7919ec047705905e76ab87b9d9d753eff55
vsabado <37255502+vsabado@users.noreply.github.com>
2019-02-18 21:14:31 -0600

    Part 1 complete

---
+++ Part1HW1/src/AESED.java
@@ -0,0 +1,107 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class AESED {
+
+    private static final String key = "aesEncryptionKey";
+    private static final String initVector = "encryptionIntVec";
+
+    public static String encrypt(String string) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(string.getBytes()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encryptedMessage) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedMessage)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void encryptCmd() {
+        System.out.println("What's your message?");
+        Scanner scan = new Scanner(System.in);
+        String m = scan.nextLine();
+        System.out.println("Message to encrypt: " + m);
+        String encryptedString = encrypt(m);
+
+        PrintStream ps = null;
+        try {
+            ps = new PrintStream("output/ctext.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ps.println(encryptedString);
+    }
+
+    private static void decryptCmd() {
+
+        try {
+            File fileDir = new File("output/ctext.txt");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileDir), StandardCharsets.UTF_8));
+
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                System.out.println("After decryption - " + decrypt(str));
+            }
+            in.close();
+        } catch (UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean loop = true;
+        while (loop) {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Would you like to encrypt or decrypt? e to encrypt, d to decrypt, q to quit");
+            char c = reader.next(".").charAt(0);
+            while (c != 'e' && c != 'd' && c != 'q') {
+                System.out.println("Invalid, please input a valid command");
+                c = reader.next(".").charAt(0);
+            }
+
+            switch (c) {
+                case 'e':
+                    encryptCmd();
+                    break;
+                case 'd':
+                    decryptCmd();
+                    break;
+                case 'q':
+                    loop = false;
+                    break;
+            }
+
+        }
+    }
+}
